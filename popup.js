df498676f93c2b9f3a0ebbb6f6e7057870ffe167
looterliam <liamtimmerman9@gmail.com>
2024-06-19 10:46:25 -0400

    Create popup.js

---
+++ popup.js
@@ -0,0 +1,14 @@
+document.getElementById('capture').addEventListener('click', () => {
+    chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+        chrome.scripting.executeScript({
+            target: {tabId: tabs[0].id},
+            files: ['content.js']
+        });
+    });
+});
+
+chrome.runtime.onMessage.addListener((message) => {
+    if (message.action === 'display') {
+        document.getElementById('response').innerText = message.response;
+    }
+});
